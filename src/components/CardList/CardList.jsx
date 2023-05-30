e661c107311c83a6fe258b17067192908abbceb8
Svetlana Govyazina <govyazina@gmail.com>
2023-05-30 21:27:07 +0300

    added show card function

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,6 +1,15 @@
+import {useContext} from "react";
+import {GlobalContext} from "../../contexts/globalContext";
+import RestCard from "../RestCard/RestCard";
+
 export const CardList = () => {
+	const {state: {list}} = useContext(GlobalContext)
+	console.log(list)
 	return (
 		<>
+			{list.map(post => <RestCard
+				{...post}
+			/>)}
 		</>
 	)
 }
