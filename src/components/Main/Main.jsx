dfa6d54b1bfa9cd12e28969a08da27b085b3dc80
Svetlana Govyazina <govyazina@gmail.com>
2023-05-31 16:20:38 +0300

    something fixed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,17 +3,15 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 export const Main = () => {
-	return (
-		<>
-			<Container maxWidth="sm">
-				<Typography textAlign="center">
-					<h1>приветики!</h1>
-				</Typography>
-
-				<Box sx={{  height: '50vh' }}>
-					<img src={'https://pbs.twimg.com/media/FuYKf33XwAAb_yb?format=jpg&name=medium'} alt={'кисуня нюхает цветочек'} height='600'/>
-				</Box>
-			</Container>
-		</>
-	)
+    return (
+        <>
+            <Container maxWidth="sm">
+                <h1>приветики!</h1>
+                <Box sx={{height: '50vh'}}>
+                    <img src={'https://pbs.twimg.com/media/FuYKf33XwAAb_yb?format=jpg&name=medium'}
+                         alt={'кисуня нюхает цветочек'} height='600'/>
+                </Box>
+            </Container>
+        </>
+    )
 }
