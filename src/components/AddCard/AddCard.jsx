577990b5910a91e0267ab03a30500bd9e410ccf2
Svetlana Govyazina <govyazina@gmail.com>
2023-05-31 14:39:51 +0300

    add card component changed

--- src/components/AddCard/AddCard.jsx
+++ src/components/AddCard/AddCard.jsx
@@ -4,6 +4,9 @@ import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
 import {useContext, useState} from "react";
 import {GlobalContext} from "../../contexts/globalContext";
+import {Grid} from "@mui/material";
+import Container from "@mui/material/Container";
+import {useNavigate} from "react-router-dom";
 
 export default function AddCard() {
     const [name, setName] = useState('')
@@ -11,8 +14,10 @@ export default function AddCard() {
     const [description, setDescription] = useState('')
     const {dispatch} = useContext(GlobalContext)
     const [validation, setValidation] = useState(false)
+    const navigate = useNavigate()
     const handleAdd = (event) => {
         event.preventDefault()
+
         const post = {
             name,
             imgLink,
@@ -28,40 +33,58 @@ export default function AddCard() {
         } else {
             setValidation(true)
         }
+        navigate('/list')
+
 
 
     }
     return (
-        <Box
-            component="form"
-            onSubmit={handleAdd}
-            sx={{
-                '& > :not(style)': {m: 1, width: '25ch'},
-            }}
-            noValidate
-            autoComplete="off"
-        >
-            <TextField
-                error={validation}
-                id="outlined-basic"
-                label="название"
-                variant="outlined"
-                onInput={e => {
-                    setName(e.target.value)
-                    setValidation(false)
-                }}/>
-            <TextField
-                id="outlined-basic"
-                label="описание"
-                variant="outlined"
-                onInput={e => setDescription(e.target.value)}/>
-            <TextField
-                id="outlined-basic"
-                label="ссылка на изображение"
-                variant="outlined"
-                onInput={e => setImgLink(e.target.value)}/>
+        <Container>
 
-            <Button variant="contained" type='submit'>Добавить</Button>
-        </Box>
+            <Box
+                component="form"
+                onSubmit={handleAdd}
+                sx={{
+                    '& > :not(style)': {m: 1, width: '25ch'},
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <Box>
+                    <h4>
+                        Добавить новый ресторан
+                    </h4>
+                    <p>
+                        введите название, небольшое описание и ссылку на изображение
+                    </p>
+                </Box>
+                <TextField
+                    required
+                    error={validation}
+                    id="outlined-basic"
+                    label="название"
+                    variant="outlined"
+                    onInput={e => {
+                        setName(e.target.value)
+                        setValidation(false)
+                    }}/>
+                <TextField
+                    id="outlined-basic"
+                    label="описание"
+                    variant="outlined"
+                    onInput={e => setDescription(e.target.value)}/>
+
+                <TextField
+                    id="outlined-basic"
+                    label="ссылка на изображение"
+                    variant="outlined"
+                    onInput={e => setImgLink(e.target.value)}/>
+
+                <Box>
+                    <Button variant="contained" type='submit'>Добавить</Button>
+                </Box>
+
+            </Box>
+        </Container>
     );
 }
